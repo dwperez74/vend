f0c46e1c4b5b0f9b06f4144480310750c0f022f6
David Perez <davidperez3611@gmail.com>
2020-10-09 09:07:55 -0400

    capstone update

--- 19_Capstone/Capstone/VendingMachine.cs
+++ 19_Capstone/Capstone/VendingMachine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Capstone
@@ -9,6 +10,19 @@ namespace Capstone
         public Dictionary<string, string> Inventory { get; private set; }
         public decimal Balance { get; private set; }
     }
+    public Dictionary<string, string> inv = new Dictionary<string, string>();
+    public void ReadFile()
+    {
+        string directory = Environment.CurrentDirectory;
+        string fileName = @"C:\Users\David Perez\Desktop\MyTE\c-module-1-capstone-team-2\19_Capstone\vendingmachine.csv";
+
+        string fullPath = Path.Combine(directory, fileName);
+
+
+        //worddictionary.add(string[], string[])
+    }
+
+
     //method to feed money
     //method for making change
     //method for purchasing snack
