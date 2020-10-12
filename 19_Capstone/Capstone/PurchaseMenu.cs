d369e27d291c94ce37b55d5660cccc09f965a718
Brad Grasl <brad.grasl@gmail.com>
2020-10-12 17:38:43 -0400

    Finished ex-tests

--- 19_Capstone/Capstone/PurchaseMenu.cs
+++ 19_Capstone/Capstone/PurchaseMenu.cs
@@ -39,12 +39,14 @@ namespace Capstone
         }
         private MenuOptionResult FinishTransaction()    //TODO: Balance keeps getting reset and thus is giving improper change. Also, we need to add an exit option once the change has been "given"
         {
+            decimal startingBalance = this.vendingMachine.Balance;
             int quarters = (int)(vendingMachine.Balance / .25M);
             vendingMachine.Balance -= (quarters * .25M);
             int dimes = (int)(vendingMachine.Balance / .10M);
             vendingMachine.Balance -= (dimes * .10M);
             int nickels = (int)(vendingMachine.Balance / .05M);
             vendingMachine.Balance -= (nickels * .05M);
+            vendingMachine.WriteLog("GIVE CHANGE:", startingBalance);
             Console.WriteLine($"Your change is {quarters} quarters, {dimes} dimes, {nickels} nickels, and the balance is now: {vendingMachine.Balance:c}.");
             return MenuOptionResult.CloseMenuAfterSelection;
             
@@ -52,6 +54,7 @@ namespace Capstone
 
         private MenuOptionResult SelectProduct()
         {
+            decimal startingBalance = this.vendingMachine.Balance;
             this.vendingMachine = vendingMachine;
             //VendingMachine vendingMachine = new VendingMachine(itemsDictionary);
             //Show list of items for user to choose from
@@ -68,7 +71,7 @@ namespace Capstone
             {
                 Products selectedProduct = vendingMachine.DispenseProduct(userSelection);
                 Console.WriteLine($"You have purchased {selectedProduct.ProductName} for {selectedProduct.Price:c} and have a remaining balance of {this.vendingMachine.Balance:c}.");
-                //Console.WriteLine(selectedProduct.);
+                vendingMachine.WriteLog($"{selectedProduct.ProductName} {selectedProduct.SlotLocation}", startingBalance);
             }
             catch (Exception ex)
             {
@@ -79,9 +82,11 @@ namespace Capstone
 
         private MenuOptionResult FeedMoney()
         {
+            decimal startingBalance = this.vendingMachine.Balance;
             Console.WriteLine("Enter Dollar Amount: ");
             decimal deposit = decimal.Parse(Console.ReadLine());
             this.vendingMachine.Balance += deposit;     //this.VendingMachine.      //This might need to be added back
+            vendingMachine.WriteLog("FEED MONEY: ", startingBalance);
             Console.WriteLine($"Your new balance is: {this.vendingMachine.Balance:c} ");
             return MenuOptionResult.WaitAfterMenuSelection;
         }
